bee064167cb0b79446895a17fe9c0214f32403fb
sajithkp <31349721+sajithkp@users.noreply.github.com>
2017-08-28 20:26:00 +0530

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int numbr = 2017;
+	float dec = 20.17;
+	bool bl = 1;
+	char cha = 's';
+	cout<<numbr<<endl<<dec<<endl<<bl<<endl<<cha;
+	// your code goes here
+	return 0;
+}
---
+++ q2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int firstnumber,secondnumber,sum;
+	cout<<"enter the first number: ";
+	cin>>firstnumber;
+	cout<<"enter the second number: ";
+	cin>>secondnumber;
+	sum=firstnumber+secondnumber;
+	cout<< "the sum of your two numbers is"<< sum << ".";
+	return 0;
+}
